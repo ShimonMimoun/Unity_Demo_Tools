9a7b947eb9c6a97b970611ef60db0585d8a13e14
ShimonMimoun <shimon1705@gmail.com>
2020-05-05 19:56:56 +0300

    FIX

---
+++ MovementAndMiniMap/Assets/Scripts/CircularMotion.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircularMotion : MonoBehaviour
+{
+    [SerializeField]
+    private float RotateSpeed = 5f, Radius = 2f;
+
+    private Vector2 _centre;
+    private float _angle;
+    private float direction = 1;
+    private bool ans = false;
+
+    private void Start()
+    {
+        _centre = new Vector2(transform.position.x, transform.position.y - Radius);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            direction = 1;
+            ans = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            direction = -1;
+            ans = true;
+        }
+        if (ans)
+        {
+            _angle += RotateSpeed * Time.deltaTime * direction;
+
+            var offset = new Vector2(Mathf.Sin(_angle), Mathf.Cos(_angle)) * Radius;
+            transform.position = _centre + offset;
+            ans = false;
+        }
+    }
+}
